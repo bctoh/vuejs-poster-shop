690b27ed31c8b7319881ea88f94b73cc09700e5b
Brian Toh <brian.toh@live.com>
2017-11-27 12:00:41 +0800

    (feat) add items and functionality to add items to cart

--- public/script.js
+++ public/script.js
@@ -1,1 +1,28 @@
-console.log('It works.');
+new Vue({
+	el: '#app',
+	data: {
+		total: 0,
+		items: [
+			{ id: 1, title: 'Item 1' },
+			{ id: 2, title: 'Item 2' },
+			{ id: 3, title: 'Item 3' }
+		],
+		cart: []
+	},
+	methods: {
+		addItem: function(index) {
+			this.total += 9.99;
+			var item = this.items[index];
+			for (var i = 0; i < this.cart.length; i++) {
+				if (this.cart[i] == item) {
+					this.cart[i].qty++;
+				}
+			}
+			this.cart.push({
+				id: item.id,
+				title: item.title,
+				qty: 1
+			});
+		}
+	}
+});
